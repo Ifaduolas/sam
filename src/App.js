4a2e89edbadf2da331c2ae1f00b14c086a6fa149
ifaduolasgit config --global user.email ifaduolas@gmail.com <ifaduolas@gmail.com>
2023-11-13 12:00:32 +0100

    thanks to Firat Atalay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react"
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+
+  async function getAdvice(){
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+    console.log(data);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+      <div>
+        <h1 className="h1-text">{advice}</h1>
+        <button className="btn" onClick={getAdvice}>Get  advice</button>
+        <Message count={count}/>
+      </div>
+      </div>
+   
   );
+}
+
+function Message(props){
+  return(
+    <p className="message">
+          You have read <strong> {props.count} </strong> pieces of advice
+        </p>
+  )
 }
 
 export default App;
